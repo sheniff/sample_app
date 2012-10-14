af022a7043d8d25ffc098fce6f2efa31977b8774
Sergio Almecija <sheniff@gmail.com>
2012-10-14 01:01:00 -0700

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "UserPages" do
+
+  subject { page }
+
+  describe "signup_pathgn up page" do
+    
+    before { visit signup_path }
+
+    it { should have_selector('h1', text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+  end
+
+end
